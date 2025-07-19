d3f85e5d4bab0f79ce277736a66de55a168df0b4
Asaki Yuki <vantrong2007vn@gmail.com>
2025-07-19 17:57:42 +0700

    idk

--- src/build.ts
+++ src/build.ts
@@ -27,7 +27,7 @@ if (!fs.existsSync("build")) fs.mkdirSync("build")
 			version: [0, 0, 1],
 			baseGameVersion: Utils.parseVersion(getVersion[version]),
 		})
-		execSync(`bun run src/app.ts --version=${version}`, { stdio: "ignore" })
+		execSync(`npx bun run ui --version=${version}`, { stdio: "ignore" })
 		fs.cpSync("Minecraft-UIBuild.mcpack", `build/${version}.mcpack`)
 
 		all_versions.push(`${version}-${getVersion[version]}`)
